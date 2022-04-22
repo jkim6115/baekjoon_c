ce16c3ac2f8537e7b9b1850068d10b2aa081976e
yhkim <jkim6115@gmail.com>
2022-04-22 10:36:26 +0900

    baekjoon 1110

--- Main.c
+++ Main.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 
-int main(){
-    int num1, num2;
+int Num = 0;
+int count = 0;
+
+int New_number(int N){
+    int N1, N10;
+    int temp;
+
+    N1 = N % 10;
+    N10 = N / 10;
+    temp = (N1 + N10) % 10;
+
+    int new_num = (N1*10) + temp;
+    count++;
     
-    while(scanf("%d%d",&num1, &num2) != EOF){        
-        printf("%d\n", num1+num2);
+    if(new_num == Num){
+        return count;
+    } else {
+        return New_number(new_num);
     }
+}
+
+int main(){
+    scanf("%d", &Num);
+    if(Num<10){
+        Num = Num*10;
+    }
+    
+    printf("%d",New_number(Num));
 
     return 0;
 }
