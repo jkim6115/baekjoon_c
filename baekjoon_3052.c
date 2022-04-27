8f8d16d54d40fe216eb651baad5ef18745e22c21
yhkim <jkim6115@gmail.com>
2022-04-27 20:54:15 +0900

    baekjoon 3052

--- baekjoon_3052.c
+++ baekjoon_3052.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int num1, num2;
     int arr[10] = {0, };
-    int count = 0;
+    int result = 0;
 
-    for(int i=0; i<=9; i++){
+    for(int i=0; i<10; i++){
         scanf("%d", &arr[i]);
         arr[i] %= 42;
     }
 
-    for(int j=0; j<10; j++){
-        for(int k=0; k<42; k++){
-            if(arr[j] == k){
-                count += 1;
-            }
+    for(int i=0; i<10; i++){
+        int count = 0;
+        for(int j=i+1; j<10; j++){
+            if(arr[i] == arr[j]){ count++; }
         }
+        if(count==0){ result++; }
     }
 
-    printf("%d", count);
+    printf("%d", result);
 
     return 0;
 }
